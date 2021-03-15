c2e3ab3fad085b49a1f2ad0ca1f1205c643594d2
Dzmitry <74104111+GiperSi@users.noreply.github.com>
2021-03-15 20:36:30 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  if (array.length == 0) { return 0; } else {
+    for (let i = 0; i < array.length; i++) { return Math.min(...array) }
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  if (array.length ==0) { return 0; } else {
+  for (let i = 0; i < array.length; i++) { return Math.max(...array) }
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  if (array.length ==0) { return 0; } else {
+    return array.reduce((accumulator, currentValue) => accumulator + currentValue) / array.length;
+  }
 }
